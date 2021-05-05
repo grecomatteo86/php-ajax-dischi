420d92e9f7c525f6d8ef0a894401de552c442b0a
Matteo <grecomatteo86@gmail.com>
2021-05-05 15:45:10 +0200

    index.php//inclusion head,header,main, footer in index.php

---
+++ partials/template/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer_section">
+    <p class="pirate_title"><small>the first pirate experience</small></p>
+    <i class="fas fa-skull"></i>
+</footer>
---
+++ partials/template/main.php
@@ -0,0 +1,29 @@
+<main class="main_section">
+    <div class="cd_container">
+    <!-- poster -->
+    <div class="poster_container pdd_btm">
+        <img src="" alt="">
+    </div>
+    <!-- /poster -->
+    <!-- title -->
+    <div class="title_cd_container pdd_btm">
+        <h2 class="second_title"></h2>
+    </div>
+    <!-- /title -->
+    <!-- author -->
+    <div class="author_container pdd_btm">
+        <h3 class="third_title"></h3>
+    </div>
+    <!-- /author -->
+    <!-- genre -->
+    <div class="genre_container pdd_btm">
+        <h4 class="genre"></h4>
+    </div>
+    <!-- /genre -->
+    <!-- year -->
+    <div class="year_container">
+        <h4 class="year"></h4>
+    </div>
+    <!-- /year -->
+    </div>
+</main>
