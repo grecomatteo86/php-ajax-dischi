abc1b126e2bf24afdeafd4df0573bd8bbe54b59b
Matteo <grecomatteo86@gmail.com>
2021-08-05 14:02:53 +0200

    index.php_cleaning

--- indexVuejsPhp/index.php
+++ indexVuejsPhp/index.php
@@ -1,32 +1,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- head_inclusion -->
 <?php include __DIR__ . '/partials/template/head.php'; ?>
-<!-- /head_inclusion -->
-
 <body>
-
     <div id="app" class="wrapper">
-
-        <!-- header_inclusion -->
         <?php include __DIR__ . '/partials/template/header.php'; ?>
-        <!-- /header_inclusion -->
-
-        <!-- main_inclusion -->
         <?php include __DIR__ . '/partials/template/main.php'; ?>
-        <!-- /main_inclusion -->
-
-        <!-- footer_inclusion -->
         <?php include __DIR__ . '/partials/template/footer.php'; ?>
-        <!-- /footer_inclusion -->
-
     </div>
-
-    <!-- script.js -->
     <script src="js/script.js" charset="utf-8"></script>
-    <!-- /script.js -->
-
 </body>
 </html>
